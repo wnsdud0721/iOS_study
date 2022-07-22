fd16bffb7b28483cca94d1148c117b33c217a21c
wnsdud0721 <wnsdud0721@naver.com>
2022-07-22 21:54:33 +0900

    banner semi final

---
+++ FrontTest/FrontTest/CollectionCell/firstCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  firstCollectionViewCell.swift
+//  FrontTest
+//
+//  Created by Junyoung_Hong on 2022/07/22.
+//
+
+import Foundation
---
+++ FrontTest/FrontTest/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  FrontTest
+//
+//  Created by Junyoung_Hong on 2022/07/22.
+//
+
+import Foundation
