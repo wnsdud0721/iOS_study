ca374c702fc690c220aed541ad84a1a49109fb06
wnsdud0721 <wnsdud0721@naver.com>
2022-08-03 01:53:38 +0900

    List2 first commit

---
+++ List2/List2/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  List2
+//
+//  Created by Junyoung_Hong on 2022/08/03.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var listImage: UIImageView!
+}
---
+++ List2/List2/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  List2
+//
+//  Created by Junyoung_Hong on 2022/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return listImage.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            as! MyCollectionViewCell
+        
+        cell.listImage.image = UIImage(named: listImage[indexPath.row])
+        cell.listImage.layer.cornerRadius = 10
+        cell.listImage.layer.borderWidth = 0.5
+        
+        return cell
+    }
+    
+
+    var listImage:[String] = ["naver12", "naver11", "naver10", "naver9", "naver8", "naver7", "naver6", "naver1"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
